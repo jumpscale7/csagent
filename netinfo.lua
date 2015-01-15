445917a38442f851929733e886660ec1bd37160c
Amr Hassan <amr.hassan@codescalers.com>
2015-01-15 14:27:06 +0200

    netinfo: Fixed wrong MAC address being returned.

--- netinfo.lua
+++ netinfo.lua
@@ -5,30 +5,43 @@ M = {}
 --
 -- When called, returns a dynamically constructed table of network devices and their information.
 --
+-- Note: Returns only information about the network devices with assigned IPv4 addresses.
+--
+-- Example output:
+-- {
+--      {ip = '127.0.0.1', cidr = 8, mac = '00:00:00:00:00:00', name = 'lo'},
+--      {ip = '10.0.2.15', cidr = 24, mac = u'08:00:27:c5:03:64', name = 'eth0'},
+--      {ip = '192.168.33.10, cidr = 24, mac = u'08:00:27:01:83:b8', name = 'eth1'}
+-- }
+--
 local function retrieve_netinfo ()
-    local info_table = {}
+
+    local function mac_address_for_interface(interface_name)
+        assert(interface_name)
+        local fp = io.open('/sys/class/net/' .. interface_name .. '/address')
+        assert(fp)
+        local address = fp:read()
+        fp:close()
+        return address
+    end
+
+    local info = {}
     for net_device in unix.getifaddrs() do
-        if net_device.addr and net_device.family == 2 or net_device.family == 10 then   -- A device listing with MAC or an IP address
-            local record = info_table[net_device.name] or {name = net_device.name}
+        if net_device.addr and net_device.family == 2 then   -- A device listing with an IP address
+            local record = {
+                name = net_device.name,
+                ip = net_device.addr,
+                cidr = net_device.prefixlen,
+                mac = mac_address_for_interface(net_device.name),
+            }
 
-            if net_device.family == 2 then
-                record.ip = net_device.addr
-                record.cidr = net_device.prefixlen
-            elseif net_device.family == 10 then
-                record.mac, _ = net_device.addr:match('[:a-zA-Z0-9]+')
-            end
-
-            info_table[net_device.name] = record
+            table.insert(info, record)
         end
     end
 
-    local info_array = {}
-    for _, value in pairs(info_table) do
-        if value.mac then -- Server only accepts entries with MAC address
-            table.insert(info_array, value)
-        end
-    end
-    return info_array
+    return info
 end
+
+
 
 return setmetatable(M, {__call = retrieve_netinfo})
