d6b5ba5ab0b13178670c2d1481a77b12518edab8
Amr Hassan <amr.hassan@gmail.com>
2015-02-25 15:46:42 +0200

    Privileged execution of CI setup script

--- .travis_setup.sh
+++ .travis_setup.sh
@@ -5,11 +5,11 @@ sudo apt-get install -y lua5.1  liblua5.1-dev liblua5.1 git
 # Install luarocks
 wget -O - http://luarocks.org/releases/luarocks-2.2.0.tar.gz | tar xz
 cd luarocks-2.2.0
-./configure && make && sudo make install
+sudo ./configure && make && sudo make install
 
 # Installing the dependencies now
 cd ~
 git clone -b rel-20140819 https://github.com/wahern/lunix.git
 cd lunix
-make all5.1 prefix=/usr/
-make install5.1 prefix=/usr/
+sudo make all5.1 prefix=/usr/
+sudo make install5.1 prefix=/usr/
