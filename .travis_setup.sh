dcdd79c097ac55e79e4cb65e5db52f80832520ac
Amr Hassan <amr.hassan@gmail.com>
2015-02-25 15:40:37 +0200

    Brand new CI setup script

--- .travis_setup.sh
+++ .travis_setup.sh
@@ -1,41 +1,15 @@
- script for setting up environment for travis-ci testing. 
-# Sets up Lua and Luarocks. 
-# LUA must be "Lua 5.1", "Lua 5.2" or "LuaJIT 2.0". 
-# Source: https://github.com/Olivine-Labs/busted/blob/master/.travis_setup.sh
-
 set -e
 
-echo 'rocks_servers = {
-  "http://rocks.moonscript.org/",
-  "http://luarocks.org/repositories/rocks"
-}' >> ~/config.lua
+sudo apt-get install -y lua5.1  liblua5.1-dev liblua5.1 git
 
-
-wget -O - https://github.com/wahern/lunix/archive/rel-20140819.tar.gz | tar xz
-
-
-if [ "$LUA" == "LuaJIT 2.0" ]; then
-  wget -O - http://luajit.org/download/LuaJIT-2.0.2.tar.gz | tar xz
-  cd LuaJIT-2.0.2
-  make && sudo make install INSTALL_TSYMNAME=lua;
-
-else
-  if [ "$LUA" == "Lua 5.1" ]; then
-    cd lunix-rel-20140819 ; make all5.1 ; cd ..
-    wget -O - http://www.lua.org/ftp/lua-5.1.5.tar.gz | tar xz
-    cd lua-5.1.5;
-  elif [ "$LUA" == "Lua 5.2" ]; then
-    cd lunix-rel-20140819 ; make all5.2 ; cd ..
-    wget -O - http://www.lua.org/ftp/lua-5.2.3.tar.gz | tar xz
-    cd lua-5.2.3;
-  fi
-  sudo make linux install;
-fi
-
-cd ..
+# Install luarocks
 wget -O - http://luarocks.org/releases/luarocks-2.2.0.tar.gz | tar xz
 cd luarocks-2.2.0
+./configure && make && sudo make install
 
-make && sudo make install
-cd ..
-
+# Installing the dependencies now
+cd ~
+git clone -b rel-20140819 https://github.com/wahern/lunix.git
+cd lunix
+make all5.1 prefix=/usr/
+make install5.1 prefix=/usr/
