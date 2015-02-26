1370caea4f4b2276df05ab34e7a7e769918e2d5b
Amr Hassan <amr.hassan@gmail.com>
2015-02-26 08:32:08 +0200

    CI setup script now properly installs dependencies

--- .travis_setup.sh
+++ .travis_setup.sh
@@ -1,6 +1,6 @@
 set -e
 
-sudo apt-get install -y lua5.1  liblua5.1-dev liblua5.1 git
+sudo apt-get install -y lua5.1  liblua5.1-dev liblua5.1 git curl
 
 # Install luarocks
 wget -O - http://luarocks.org/releases/luarocks-2.2.0.tar.gz | tar xz
@@ -14,4 +14,14 @@ cd lunix
 sudo make all5.1 prefix=/usr/
 sudo make install5.1 prefix=/usr/
 
-# TODO: parse dependencies.txt and install via luarocks
+
+# Get the dependencies list and install those marked with a luarocks source using luarocks
+for dep in `curl https://raw.githubusercontent.com/Jumpscale/csagent/master/dependencies.txt | sed -e1d `; do
+  pkg_name=`echo $dep | cut -d, -f2`
+  source=`echo $dep | cut -d, -f1`
+  pkg_version=`echo $dep | cut -d, -f3`
+  if [ "$source" = "luarocks" ]; then
+    echo "Installing ${pkg_name} ${pkg_version}"
+    sudo luarocks install --server=http://rocks.moonscript.org/manifests/amrhassan ${pkg_name} ${pkg_version}
+  fi
+done
